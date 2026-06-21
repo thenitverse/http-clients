1b647d5acec24a09aeabe0811f703f73829780b2
nitika2205 <knitika567@gmail.com>
2026-06-21 20:59:41 +0530

    main file for net package in http.

---
+++ net_package.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getIssueData(url string) ([]byte, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %w", err)
+	}
+
+	return data, nil
+}
+
+func prettify(data string) (string, error) {
+	var prettyJSON bytes.Buffer
+	err := json.Indent(&prettyJSON, []byte(data), "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("error indenting JSON: %w", err)
+	}
+	return prettyJSON.String(), nil
+}
