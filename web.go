b5020adc37b14e2b060ccc1f51d7fbe60be809ff
nitika2205 <knitika567@gmail.com>
2026-06-21 20:53:19 +0530

    main file for communication in web.

---
+++ web.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	issues, err := getIssueData()
+	if err != nil {
+		log.Fatalf("error getting issue data: %v", err)
+	}
+
+	e := string(issues)
+	fmt.Println(e)
+}
